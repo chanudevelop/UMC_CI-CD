e8c36a126c7d763b542383ec77c31da4e16d7e58
chanudevelop <chanu.develop@gmail.com>
2025-06-30 23:30:05 +0900

    🎉 초기 Spring 프로젝트 세팅

---
+++ src/test/java/umc_ci_cd/beanstalk/BeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package umc_ci_cd.beanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
